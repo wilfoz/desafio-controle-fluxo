02f039d67c0b0522ed156ba26e6fd03a7378e6b6
wilfoz <wil_foz@yahoo.com.br>
2022-08-28 15:56:28 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
